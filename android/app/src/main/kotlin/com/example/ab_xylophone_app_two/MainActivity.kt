6049a44874ad49af5d454c1fd5ebb0e2f256e3d9
carlstep <carlstep333@me.com>
2021-05-21 08:17:22 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/ab_xylophone_app_two/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ab_xylophone_app_two
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  void buildKey() {
+    Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: Colors.red),
+        onPressed: () {
+          playSound(1);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
